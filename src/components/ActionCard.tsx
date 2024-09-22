b5f47a8e2e192837556d8a79227282a615296aed
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2024-09-22 11:44:33 +0530

    ActionCard: Remove description field.

--- src/components/ActionCard.tsx
+++ src/components/ActionCard.tsx
@@ -1,20 +1,18 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import CardActionArea from '@mui/material/CardActionArea';
-import CardActions from '@mui/material/CardActions';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardActions from "@mui/material/CardActions";
 
 type ActionCardProp = {
-  imageURL: string, 
-  title: string,
-  description: string,
+  imageURL: string;
+  title: string;
+};
 
-}
-
-export default function ActionCard({imageURL, title, description}: ActionCardProp) {
+export default function ActionCard({ imageURL, title }: ActionCardProp) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
@@ -27,9 +25,6 @@ export default function ActionCard({imageURL, title, description}: ActionCardPro
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
             {title}
-          </Typography>
-          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-            {description}
           </Typography>
         </CardContent>
       </CardActionArea>
